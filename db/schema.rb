a7a6f856d377fd1e396a69bee48ba5e83905ad4c
EC2 Default User <138359651+iamkassan@users.noreply.github.com>
2023-07-15 11:05:38 +0000

    課題２、投稿フォーム完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_162621) do
+ActiveRecord::Schema.define(version: 2023_07_15_101408) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
