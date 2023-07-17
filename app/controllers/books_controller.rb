bcc9d06c5060ae00f92df5548b57e86351a69569
EC2 Default User <138359651+iamkassan@users.noreply.github.com>
2023-07-17 14:01:35 +0000

    Rspecテスト実施

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,10 @@ class BooksController < ApplicationController
    if @book.save
 
     flash[:notice] = "Book was succesfully created."
-    
+
     redirect_to book_path(@book.id)
   else
-     @books = Book.all
+  @books = Book.all
   render :index
 end
   end
@@ -28,12 +28,20 @@ end
   def edit
     @book = Book.find(params[:id])
   end
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
 
     flash[:notice] = "Book was succesfully updated."
-    redirect_to book_path(book.id)
+
+    redirect_to book_path(@book.id)
+  else
+
+     render :edit
+  end
+
   end
 
   def destroy
