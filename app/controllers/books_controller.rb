a0f49e9c5a9317f146a56e8bfe463b2d42d9f4db
EC2 Default User <138359651+iamkassan@users.noreply.github.com>
2023-07-17 10:27:18 +0000

    バリテーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,16 @@ class BooksController < ApplicationController
   @book = Book.new
   end
   def create
-  # １.&2. データを受け取り新規登録するためのインスタンス作成
+  
     book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4.詳細画面へリダイレクト
-    redirect_to book_path(book.id)#book_path(book.id)に変更する！
+   if book.save
+    
+    flash[:notice] = "Book was succesfully created."
+    redirect_to book_path(book.id)
+  else
+  @books = Book.all
+  render "new"
+end
   end
 
   def index
@@ -26,9 +30,17 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)#{book.id}"
-    #book_path(book.id)に変更する！
+    
+    flash[:notice] = "Book was succesfully updated."
+    redirect_to book_path(book.id)
   end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+    
   private
   # ストロングパラメータ
   def book_params
