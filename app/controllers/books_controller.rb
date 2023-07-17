2c00cc57c80a8fb9f0f85b139b2a7aed4a4c9483
EC2 Default User <138359651+iamkassan@users.noreply.github.com>
2023-07-17 12:48:41 +0000

    メンター町田さんにバリデーションのエラーをご教示いただく

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,21 +3,22 @@ class BooksController < ApplicationController
   @book = Book.new
   end
   def create
-  
-    book = Book.new(book_params)
-   if book.save
-    
+
+    @book = Book.new(book_params)
+   if @book.save
+
     flash[:notice] = "Book was succesfully created."
-    redirect_to book_path(book.id)
+    
+    redirect_to book_path(@book.id)
   else
-  @books = Book.all
-  render "new"
+     @books = Book.all
+  render :index
 end
   end
 
   def index
     @book = Book.new
-    @books = Book.all 
+    @books = Book.all
   end
 
   def show
@@ -30,17 +31,17 @@ end
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    
+
     flash[:notice] = "Book was succesfully updated."
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-    
+
   private
   # ストロングパラメータ
   def book_params
